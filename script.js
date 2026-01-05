1680cf87109ce82c69aaf4c2427b0864350d051b
nurdo2009 <wertbvjcc@gmail.com>
2026-01-05 15:04:45 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let img = document.getElementById('img')
+
+let Clict = () => {
+img.setAttribute('src','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvFBa3G11OUBYADP7ouSBgwiiRzSYorF4dfg&s')
+}
